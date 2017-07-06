3f119bbc63f49b5178ad44b4d399f10bfb9a01ce
pallat <yod.pallat@gmail.com>
2017-07-06 23:34:19 +0700

    ensure it works for 10 queue

---
+++ queue.go
@@ -0,0 +1,45 @@
+package queue
+
+func NewQueue(total int) *Queue {
+	ch := make(chan int, total)
+	for i := 0; i < total; i++ {
+		ch <- i
+	}
+
+	q := &Queue{
+		i:          ch,
+		pop:        make(chan int),
+		fullNotify: make(chan struct{}),
+	}
+	go q.background()
+	return q
+}
+
+type Queue struct {
+	i          chan int
+	pop        chan int
+	fullNotify chan struct{}
+}
+
+func (q *Queue) background() {
+	for {
+		q.pop <- <-q.i
+		if len(q.i) == 0 {
+			q.fullNotify <- struct{}{}
+		}
+	}
+}
+
+func (q *Queue) Pop() <-chan int {
+	return q.pop
+}
+
+func (q *Queue) Full() <-chan struct{} {
+	return q.fullNotify
+}
+
+func (q *Queue) Close() {
+	// close(q.i)
+	// close(q.pop)
+	close(q.fullNotify)
+}
---
+++ queue_test.go
@@ -0,0 +1,36 @@
+package queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCountChanQueue(t *testing.T) {
+	q := NewQueue(10)
+	chCount := make(chan int)
+	go func(ch chan int) {
+		count := 0
+		for {
+			select {
+			case i := <-q.Pop():
+				fmt.Println("first", i)
+				count++
+			case i := <-q.Pop():
+				fmt.Println("second", i)
+				count++
+			case i := <-q.Pop():
+				fmt.Println("third", i)
+				count++
+			case <-q.Full():
+				fmt.Println("quit")
+				ch <- count
+			}
+		}
+	}(chCount)
+
+	count := <-chCount
+
+	if count != 10 {
+		t.Error("it should return only 10 times but got", count)
+	}
+}
